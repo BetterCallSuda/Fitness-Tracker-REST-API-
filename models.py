e424d66e184113bb6e285905b11eb5a624ffc380
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-27 20:12:15 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,3 +3,22 @@ from datetime import datetime
 
 db = SQLAlchemy()
 
+# -----------------------------
+# USER MODEL
+# -----------------------------
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+
+    workouts = db.relationship("Workout", back_populates="user", cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "email": self.email
+        }
+
