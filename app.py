45615e52604b82582204821cbe8ca563168e72a2
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-27 20:13:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,21 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+
+# -----------------------------
+# CREATE USER
+# -----------------------------
+@app.route("/users", methods=["POST"])
+def create_user():
+    data = request.get_json()
+
+    user = User(
+        name=data["name"],
+        email=data["email"]
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.to_dict()), 201
