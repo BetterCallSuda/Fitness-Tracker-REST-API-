59c1cd89bdb3f72634863c9fbb69d46e930be09f
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-02-27 20:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 from flask import Flask, request, jsonify
 from models import db, User, Workout
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///fitness.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
